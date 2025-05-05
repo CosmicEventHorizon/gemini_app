4f1e6385dc491c4afd05584be81adce6004e43a1
Blood-Pirouette <bloodpirouette@tutanota.com>
2025-05-05 10:52:15 -0400

    Added config.py to gitignore

--- retriever/embedding_model.py
+++ retriever/embedding_model.py
@@ -11,19 +11,18 @@ import base64
 import tempfile
 from google import genai
 from google.genai.types import EmbedContentConfig
+from config import PROJECT_ID
+from config import GOOGLE_APPLICATION_CREDENTIALS_BASE64
 
-#Google authentication requires the project_id and the service account key
-project_id = os.environ["PROJECT_ID"]
-creds_data = os.environ["GOOGLE_APPLICATION_CREDENTIALS_BASE64"]
 
 #the json key is encoded as base64 key which is decoded here back to a json file
 key_path = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
-key_path.write(base64.b64decode(creds_data))
+key_path.write(base64.b64decode(GOOGLE_APPLICATION_CREDENTIALS_BASE64))
 key_path.close()
 
 #environment variables to be used for authentication
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path.name
-os.environ["GOOGLE_CLOUD_PROJECT"] = project_id
+os.environ["GOOGLE_CLOUD_PROJECT"] = PROJECT_ID
 os.environ["GOOGLE_CLOUD_LOCATION"] = "global"
 os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "true"
 
